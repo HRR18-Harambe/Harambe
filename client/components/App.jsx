6eba256a694964cb724886a78ec31336c1cfa9da
Jason Holtkamp <holtkam2@gmail.com>
2016-10-01 14:21:45 -0500

    add URL click now passes a url and a button name to reducer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,13 +3,15 @@ import Setting from '../containers/SettingContainer';
 import SettingDisp from '../containers/SettingsDispContainer.js';
 import RSSHolder from '../containers/RSSHolder';
 import SearchBar from '../components/SearchBar';
+import ButtonList from '../components/ButtonList';
+import LinkButton from '../components/LinkButton';
 
 const App = () => (
   <div>
     <RSSHolder />
     <SearchBar />
+    <ButtonList />
     <Setting />
-    <h1>Welcome, User</h1>
     <SettingDisp />
   </div>
 );
