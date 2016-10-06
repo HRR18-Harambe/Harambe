302e753c468f1ec67e6c70090ddbc2a7e38762fd
Patrik Korzinski <patrik.korzinski@gmail.com>
2016-10-06 12:06:11 -0700

    (feat) made greeting dynamic and split into own component.

--- client/containers/welcomeBarContainer.js
+++ client/containers/welcomeBarContainer.js
@@ -5,12 +5,4 @@ const mapStateToProps = state => ({
   welcomeBarState: state.currentUser,
 });
 
-<<<<<<< 74990cc81d136d47e8809ed78ea8e38f448673fb
-<<<<<<< df0d5b2ac87c299fb62ce478233eab46453fbd32
 export default connect(mapStateToProps)(WelcomeBar);
-=======
-export default connect(mapStateToProps, null)(WelcomeBar);
->>>>>>> (feat) made greeting dynamic and split into own component.
-=======
-export default connect(mapStateToProps)(WelcomeBar);
->>>>>>> ???
