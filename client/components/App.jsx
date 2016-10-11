e537a0bd4e197db2cf96fb085dfc7622c589b432
Patrik Korzinski <patrik.korzinski@gmail.com>
2016-10-11 10:42:42 -0700

    (feat) watsonbar functionality complete.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -37,9 +37,7 @@ const App = () => (
       </div>
 
       <Paper className="bottomNav" zDepth={1}>
-        <div className="Sentiment">
-          <Senti />
-        </div>
+        <Senti />
         <SettingsDispContainer />
         <About />
         <ThemeButton />
--- client/components/SentiBar.jsx
+++ client/components/SentiBar.jsx
@@ -1,23 +1,17 @@
 import React, { PropTypes } from 'react';
 
-
 const SentiBar = ({ SentiBarState }) => (
   // SentiBarState has the object with anger fear bla bla bla
-  <div>
-    <div>
-      {SentiBarState.anger}
+  <div className="Sentiment">
+    <div className="emoAnger" style={{ width: `${SentiBarState.anger * 100}%` }}>
     </div>
-    <div>
-      {SentiBarState.disgust}
+    <div className="emoDisgust" style={{ width: `${SentiBarState.disgust * 100}%` }}>
     </div>
-    <div>
-      {SentiBarState.fear}
+    <div className="emoFear" style={{ width: `${SentiBarState.fear * 100}%` }}>
     </div>
-    <div>
-      {SentiBarState.joy}
+    <div className="emoJoy" style={{ width: `${SentiBarState.joy * 100}%` }}>
     </div>
-    <div>
-      {SentiBarState.sadness}
+    <div className="emoSadness" style={{ width: `${SentiBarState.sadness * 100}%` }}>
     </div>
   </div>
 );
