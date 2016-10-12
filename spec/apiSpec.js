f41d3ffe18d34cedaba029f1ec3d57e5e4af25ed
Patrik Korzinski <patrik.korzinski@gmail.com>
2016-10-11 17:30:49 -0700

    (test) More boilerplate code

--- spec/apiSpec.js
+++ spec/apiSpec.js
@@ -9,55 +9,79 @@ const request = require('supertest');
 describe('integration tests', () => {
   describe('watson api', () => {
     it('api key working', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
     it('responds to query', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
   });
 
   describe('stormpath api', () => {
     it('api key working', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
     it('responds to query', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
   });
 
   describe('google geocoding api', () => {
     it('api key working', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
     it('responds to query', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
   });
 
   describe('openweather api', () => {
     it('api key working', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
     it('responds to query', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
   });
 
   describe('darksky api', () => {
     it('api key working', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
     it('responds to query', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
   });
 
   describe('yahoo query language api', () => {
     it('api key working', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
     it('responds to query', (done) => {
-
+      request()
+        .get()
+        .expect()
     });
   });
 });
