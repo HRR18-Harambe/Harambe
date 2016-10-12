afba78981291928373af119a396a481627888b52
Patrik Korzinski <patrik.korzinski@gmail.com>
2016-10-11 17:28:16 -0700

    (test) added boilerplate structure for integration testing.

---
+++ spec/apiSpec.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const should = require('chai').should();
+const request = require('supertest');
+
+// Watson, stormpath, google maps, openweather, darksky, yql
+describe('integration tests', () => {
+  describe('watson api', () => {
+    it('api key working', (done) => {
+
+    });
+    it('responds to query', (done) => {
+
+    });
+  });
+
+  describe('stormpath api', () => {
+    it('api key working', (done) => {
+
+    });
+    it('responds to query', (done) => {
+
+    });
+  });
+
+  describe('google geocoding api', () => {
+    it('api key working', (done) => {
+
+    });
+    it('responds to query', (done) => {
+
+    });
+  });
+
+  describe('openweather api', () => {
+    it('api key working', (done) => {
+
+    });
+    it('responds to query', (done) => {
+
+    });
+  });
+
+  describe('darksky api', () => {
+    it('api key working', (done) => {
+
+    });
+    it('responds to query', (done) => {
+
+    });
+  });
+
+  describe('yahoo query language api', () => {
+    it('api key working', (done) => {
+
+    });
+    it('responds to query', (done) => {
+
+    });
+  });
+});
